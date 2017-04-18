c2a835fa251e0970301800cd792f808d2e56358b
Will Tang <whitmenn@gmail.com>
2017-04-18 11:41:04 +0800

    Updated server playing code to be non-blocking.

--- server.py
+++ server.py
@@ -17,29 +17,31 @@ def cleanup():
     print("\nBASS freed...")
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route("/version")
+@app.route("/", methods=['GET'])
+@app.route("/version", methods=['GET'])
 def version():
     return "LambdaHubServer v0.1"
 
-@app.route("/play/<path:soundfile>")
+@app.route("/play/<path:soundfile>", methods=['GET'])
 def play(soundfile):
     print('Opening sound file: %s' % soundfile)
-    print('String type: %s' % type(soundfile))
-    soundfile = soundfile.encode('ascii','ignore')
-    print('String type: %s' % type(soundfile))
-    handle = BASS_StreamCreateFile(False, soundfile, 0, 0, 0)
-    play_handle(handle, show_tags=False)
-    result = ''
-    if BASS_ErrorGetCode() != BASS_OK:
-        result ='BASS_ChannelPlay error: %s' % get_error_description(BASS_ErrorGetCode())
-    else:
-        result = "Done playing..."
+    # print('String type: %s' % type(soundfile))
+    soundfile = soundfile.encode('ascii', 'ignore')
+    # print('String type converted: %s' % type(soundfile))
+    handle = BASS_StreamCreateFile(False, soundfile, 0, 0, BASS_STREAM_AUTOFREE)
+    # play_handle(handle, show_tags=False)
 
-    return result
+    channel_info = BASS_CHANNELINFO()
+    if not BASS_ChannelGetInfo(handle, channel_info):
+        return 'BASS_ChannelGetInfo error: %s' % get_error_description(BASS_ErrorGetCode())
+
+    channel_length = BASS_ChannelGetLength(handle, BASS_POS_BYTE)
+    channel_position = BASS_ChannelGetPosition(handle, BASS_POS_BYTE)
+
+    if not BASS_ChannelPlay(handle, False):
+        return 'BASS_ChannelPlay error: %s' % get_error_description(BASS_ErrorGetCode())
+
+    return 'Playing...'
 
 
 if __name__ == "__main__":
